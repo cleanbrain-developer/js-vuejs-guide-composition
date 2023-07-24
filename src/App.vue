2a423eb88bd81ea8953c62934f3b4fbaaa6acb6d
clean_brain <clean_brain@nandsoft.co.kr>
2023-07-24 17:01:46 +0900

    2023-07-24 : 13_Essentials_Watchers 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,58 @@
 <template>
+  <p>
+    Ask a yes/no question:
+    <input v-model="question" />
+  </p>
+  <p>{{ answer }}</p>
+
+  <br/>
+  <br/>
+
+  <button @click="x++">x++</button>
+  <button @click="y++">y++</button>
 </template>
 
 <script setup>
+import { ref, watch } from 'vue'
+
+const question = ref('')
+const answer = ref('Questions usually contain a question mark. ;-)')
+
+// watch works directly on a ref
+watch(question, async (newQuestion) => {
+  if (newQuestion.indexOf('?') > -1) {
+    answer.value = 'Thinking...'
+    try {
+      const res = await fetch('https://yesno.wtf/api')
+      answer.value = (await res.json()).answer
+    } catch (error) {
+      answer.value = 'Error! Could not reach the API. ' + error
+    }
+  }
+})
+
+// ========================================================================
+
+const x = ref(0)
+const y = ref(0)
+
+// single ref
+watch(x, (newX) => {
+  console.log(`x is ${newX}`)
+})
+
+// getter
+watch(
+    () => x.value + y.value,
+    (sum) => {
+      console.log(`sum of x + y is: ${sum}`)
+    }
+)
+
+// array of multiple sources
+watch([x, () => y.value], ([newX, newY]) => {
+  console.log(`x is ${newX} and y is ${newY}`)
+})
 </script>
 
 <style>
