5718366dba31f0fc03a16ac3f25b02b71ee6cdcc
clean_brain <clean_brain@nandsoft.co.kr>
2023-07-24 16:04:11 +0900

    2023-07-24 : 08_Essentials_Conditional_Rendering 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-
+  <h1 v-if="awesome">Vue is awesome!</h1>
+  <h1 v-else>Oh no 😢</h1>
+  <button @click="awesome = !awesome">Toggle</button>
+  <template v-if="ok">
+    <h1>Title</h1>
+    <p>Paragraph 1</p>
+    <p>Paragraph 2</p>
+  </template>
+  <button @click="ok = !ok">Toggle</button>
 </template>
 
-<script>
+<script setup>
+import {ref} from "vue";
 
-export default {
-  name: 'App',
-  components: {
-  }
-}
+const awesome = ref(true)
+const ok = ref(true)
 </script>
 
 <style>
