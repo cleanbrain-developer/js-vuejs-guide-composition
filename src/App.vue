be6c8b0bce48dc0a207eb9bdde7a1bcad82c4562
clean_brain <clean_brain@nandsoft.co.kr>
2023-07-24 16:54:55 +0900

    2023-07-24 : 12_Essentials_Lifecycle_Hooks 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
+  <div></div>
 </template>
 
 <script setup>
+import {onMounted} from 'vue'
+
+onMounted(() => {
+  console.log(`the component is now mounted.`)
+})
 </script>
 
 <style>
