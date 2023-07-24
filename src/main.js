16e1e5069efefe881581b53897314bed2b1fcaa1
clean_brain <clean_brain@nandsoft.co.kr>
2023-07-24 14:24:27 +0900

    2023-07-24 : 03_Essentials_Creating_an_Application 커밋 1. 애플리케이션 인스턴스 설정 예시

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+let elementApp = createApp(App);
+// 애플리케이션 인스턴스 설정
+elementApp.config.errorHandler = (err) =>  {
+    console.log(err)
+}
+elementApp.mount('#app')
