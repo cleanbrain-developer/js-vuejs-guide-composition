5c1974ba57a30f3a5156f7f8110081a898b8d366
clean_brain <clean_brain@nandsoft.co.kr>
2023-07-24 16:51:46 +0900

    2023-07-24 : 11_Essentials_Form_Input_Bindings 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,86 @@
 <template>
+  <p>Message is: {{ message }}</p>
+
+  <br/>
+
+  <span>Multiline message is:</span>
+
+  <br/>
+
+  <p style="white-space: pre-line;">{{ message }}</p>
+
+  <br/>
+
+  <textarea v-model="message" placeholder="add multiple lines"></textarea>
+
+  <br/>
+  <br/>
+
+  <input v-model="message" placeholder="edit me" />
+
+  <br/>
+  <br/>
+
+  <input type="checkbox" id="checkbox" v-model="checked" />
+  <label for="checkbox">{{ checked }}</label>
+
+  <br/>
+  <br/>
+
+  <div>Checked names: {{ checkedNames }}</div>
+
+  <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+  <label for="jack">Jack</label>
+
+  <input type="checkbox" id="john" value="John" v-model="checkedNames">
+  <label for="john">John</label>
+
+  <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+  <label for="mike">Mike</label>
+
+  <br/>
+  <br/>
+
+  <div>Picked: {{ picked }}</div>
+
+  <input type="radio" id="one" value="One" v-model="picked" />
+  <label for="one">One</label>
+
+  <input type="radio" id="two" value="Two" v-model="picked" />
+  <label for="two">Two</label>
+
+  <br/>
+  <br/>
+
+  <div>Selected: {{ selected }}</div>
+
+  <select v-model="selected">
+    <option disabled value="">Please select one</option>
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+
+  <br/>
+  <br/>
+
+  <div>Selected: {{ selected }}</div>
+
+  <select v-model="selected" multiple>
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
 </template>
 
 <script setup>
+import {ref} from "vue";
+
+const message = ref('')
+const checked = ref(true)
+const checkedNames = ref([])
+const picked = ref('')
+const selected = ref('')
 </script>
 
 <style>
