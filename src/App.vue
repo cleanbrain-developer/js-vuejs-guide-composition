0e018a2815409b30b1b1896f1bac8205e3b93729
clean_brain <clean_brain@nandsoft.co.kr>
2023-07-24 15:43:56 +0900

    2023-07-24 : 06_Essentials_Computed_Properties 커밋 1. template 로직 단순화(computed 및 function 호출)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
+<!--template 로직 단순화-->
+<!--<template>
+  <p>Has published books:</p>
+  <span>{{ author.books.length > 0 ? 'Yes' : 'No' }}</span>
+</template>-->
+<!--computed() 함수 사용, 값이 바뀌기전까진 caching 처리-->
+<!--<template>
+  <p>Has published books:</p>
+  <span>{{ publishedBooksMessage }}</span>
+</template>-->
+<!--function 호출, 호출시마다 계산로직 처리-->
 <template>
-
+  <p>Has published books:</p>
+  <span>{{ calculateBooksMessage() }}</span>
 </template>
 
-<script>
+<script setup>
+import {reactive} from "vue";
 
-export default {
-  name: 'App',
-  components: {
-  }
+const author = reactive({
+  name: 'John Doe',
+  books: [
+    'Vue 2 - Advanced Guide',
+    'Vue 3 - Basic Guide',
+    'Vue 4 - The Mystery'
+  ]
+})
+
+// const publishedBooksMessage = computed(() => {
+//   return author.books.length > 0 ? 'Yes' : 'No'
+// })
+
+function calculateBooksMessage() {
+  return author.books.length > 0 ? 'Yes' : 'No'
 }
 </script>
 
