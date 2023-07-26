e66bd37bafe4e76cf1abd1ebb01a5881dedbd512
clean_brain <clean_brain@nandsoft.co.kr>
2023-07-26 09:17:45 +0900

    2023-07-26 : 15_Essentials_Components_Basics 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
+  <ButtonCounter/>
+  <BlogPost title="Temp Title"/>
 </template>
 
 <script setup>
+
+import ButtonCounter from "@/components/ButtonCounter";
+import BlogPost from "@/components/BlogPost";
 </script>
 
 <style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,12 @@
+<template>
+  <h4>{{ title }}</h4>
+</template>
+
+<script setup>
+// eslint-disable-next-line no-undef
+defineProps(['title'])
+</script>
+
+<style scoped>
+
+</style>
